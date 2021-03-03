32b5f2148af207145977a25a1dda1fcd215cad02
Gabriel Comonian Porto <74382419+Portolost@users.noreply.github.com>
2021-03-03 14:13:26 -0300

    03/02/2021

--- avatar.js
+++ avatar.js
@@ -1,5 +1,6 @@
 let nomeAvatarIn = document.querySelector("#input-avatar-nome");
 let corAvatarIn = document.querySelector("#input-avatar-cor");
+let cabeloAvatarIn = document.querySelector("selecionar-cabelo");
 
 nomeAvatarIn.addEventListener('input', function(){
     let nomeAvatarEl = document.querySelector("#avatar-nome");
@@ -13,3 +14,8 @@ corAvatarIn.addEventListener('input', function(){
     corpoAvatarEl.style.backgroundColor = corAvatarIn.value;
     cabecaAvatarEl.style.backgroundColor = corAvatarIn.value;
 });
+function cabelo(){
+    let cabeloAvatarEl = document.querySelector("avatar-cabelo");
+    let cabelo = cabeloAvatarIn.options[cabeloAvatarIn.selectedIndex].value;
+    cabeloAvatarEl.src = cabelo;
+};
